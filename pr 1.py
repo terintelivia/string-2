92cc04a75b1de40a69d7a5822e3de801d91d4225
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-12-04 17:52:07 +0200

    Add files via upload

---
+++ pr 1.py
@@ -0,0 +1,5 @@
+""" Se citeste un cuvant si o litera. Sa se afiseze cuvantul alcatuit prin inlocuirea fiecarui caracter al cuvantului cu litera data. """
+cuvant= input("introdu un cuvant:")
+litera= input("introdu o litera:")
+pentru i în cuvant:
+    print(a. înlocuiți (cuvant [i],litera))
