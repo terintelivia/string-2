73394b9a2faa4ec99ace771ad7f57f4b110e3dcd
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-12-04 18:51:51 +0300

    Delete pr 1.py

--- pr 1.py
+++
@@ -1,5 +0,0 @@
-""" Se citeste un cuvant si o litera. Sa se afiseze cuvantul alcatuit prin inlocuirea fiecarui caracter al cuvantului cu litera data. """
-cuvant= input("introdu un cuvant:")
-litera= input("introdu o litera:")
-for i în cuvant:
-    print(cuvant. înlocuiți (cuvant ,i))
